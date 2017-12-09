9bdc130c00344df5597e776d36020d412b9d2125
Humza Joseph <humza1born@gmail.com>
2017-12-09 23:56:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // write your code below!
 
 var name = "Humza"
-var height = "5"
+var height = 5
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height")
 console.log(parseInt(height)+1)
+ 
